3903e7b8f73dd431cfed72a999ff27d8a9ea6715
00cean11 <oceanisnotsea@gmail.com>
2022-10-13 16:22:55 -0700

    q1

--- q1.cpp
+++ q1.cpp
@@ -5,6 +5,26 @@ using namespace std;
 //********************
 // Declare Function Prototypes
 //********************
+void getinput(int &num1, int &num2){
+	cout << "Input 2 normal integar number\n";
+	cin >> num1 >> num2; 
+}
+void getinput(float &fnum1, float &fnum2){
+	cout << "Input 2 float integar number\n";
+	cin >> fnum1 >> fnum2;
+}
+void swapTwoValues(int &num1, int &num2){
+	int Temp;
+	Temp = num1;
+	num1 = num2;
+	num2 = Temp;
+}
+void swapTwoValues(float &fnum1, float &fnum2){
+	float Temp;
+	Temp = fnum1;
+	fnum1 = fnum2;
+	fnum2 = Temp;
+}
 
 int main()
 {
@@ -17,6 +37,7 @@ int main()
 
 	// -- Call overloaded function with float parameters
 	getinput(fnum1, fnum2);
+	cout << fixed << setprecision(2);
 	cout << setw(5) << fnum1 << setw(5) << fnum2 << setw(5) << endl;
 	swapTwoValues(fnum1, fnum2);
 	cout << setw(5) << fnum1 << setw(5) << fnum2 << setw(5) << endl;
